ec72cc3a137a4347c04bf16bb9f6440a724fa3e2
Phantom <fay.ashbro@gmail.com>
2020-11-07 17:55:51 -0500

    brought in browser router into the app.js to create an single page app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Search from "./pages/Search"; 
+import Saved from "./pages/Saved";
 
 
 
 function App() {
   return (
-    <div >
-      <h1> Google Books</h1>
-    </div>
+    <Router>
+      <div >
+        <h1> Google Books</h1>
+        <Switch>
+        <Route exact path ="/saved" component={Saved} />
+          <Route exact path ="/" component={Search} />
+          <Route  component={NoMatch} />
+
+        </Switch>
+
+      </div>
   );
+
+    </Router>
+
 }
 
 export default App;
