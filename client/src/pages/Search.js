6848e40f494b384ef6b8f2a9f72a2c39a61cc89e
Phantom <fay.ashbro@gmail.com>
2020-11-08 19:17:19 -0500

    brought in components into the search to create the layout with hardcoded data

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import Results from "../components/Results";
+import Form from "../components/Form";
+
 
 const Search = () => {
+
     return (
         <div>
-            <h1> Search Page. </h1>
+            <h1> Search Page. </h1>            
+            <div className={{borderStyle}}>
+                <Form/>
+            </div>
+            
+            <Results />
+        
+
         </div>
     );
 };
 
 export default Search;
+
+const borderStyle = {
+    borderColor: "black", 
+    borderWidth: "3px", 
+    backgroundColor: "lightGray", 
+    fontFamily: "courier", 
+    borderStyle: "solid"
+
+};
